917e161e7d7c3f1c6976fd03ee1490fff1470e76
Justin Pelletier <alifeofdesign@yahoo.com>
2016-06-14 00:12:22 +0000

    added users show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,7 +6,7 @@ class ProfilesController < ApplicationController
   end
   
   def create
-    @user = User.find( params[user_id] )
+    @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
     if @profile.save
       flash[:success] = "Profile Updated!"
@@ -19,6 +19,5 @@ class ProfilesController < ApplicationController
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
-  
     end
 end
